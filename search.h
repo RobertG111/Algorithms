8ccf851b6d8195d8affa2d650cedac0720ef89f6
Robert <33911814+RobertG111@users.noreply.github.com>
2022-01-10 22:18:49 +0000

    Add files via upload
    Started search library in C

---
+++ search.h
@@ -0,0 +1,10 @@
+//
+// Created by RobertG111.
+//
+
+#ifndef C_PROGRAMMING_SEARCH_H
+#define C_PROGRAMMING_SEARCH_H
+
+int BinarySearch(int target, const int *list, int size);
+
+#endif //C_PROGRAMMING_SEARCH_H
